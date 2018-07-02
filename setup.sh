a4b17d7f6e1aa5c9cfdaf32e1da6a1b3dd13db62
LeonardoGaldino <lcgm@cin.ufpe.br>
2018-07-02 16:03:44 -0300

    Minor fixes on setup script

--- setup.sh
+++ setup.sh
@@ -1,16 +1,17 @@
 APT_CMD=$(which apt)
 BREW_CMD=$(which brew)
 
-if [[ ! -z $APT_CMD ]]; then
+if [[ ! -z $BREW_CMD ]]; then
+ 	echo "Using brew to install dependencies"
+ 	sudo brew install cabal-install
+elif [[ ! -z $APT_CMD ]]; then
 	echo "Using apt to install dependencies"
 	sudo apt-get update -y
 	sudo apt-get install cabal-install freeglut3 freeglut3-dev -y
-	cabal update
-	cabal install fungen .
-elif [[ ! -z $BREW_CMD ]]; then
- 	echo "Using brew to install dependencies"
- 	sudo brew install cabal-install
 else
     echo "Error: Can't detect package installer"
     exit 1;
 fi
+
+cabal update
+cabal install fungen .
